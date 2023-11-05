b3c596a8c007cb102f783843681a6a9f3e8d62f9
gitaa <nagitaaliya07@gmail.com>
2023-11-05 13:48:27 +0700

    streamlit

---
+++ countdown.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import time
+
+st.write("""
+         # Countdown Timer
+         Input your time (in seconds) and start the countdown!
+""")
+
+x = st.number_input("Time: ", 0)
+start = st.button("Start Countdown")
+stop = ("Stop Countdown")
+
+if start:
+    while True:
+        st.write(x)
+        x -= 1
+        time.sleep(1)
+        if x == 0:
+            st.success("Countdown Finished!")
+            break
+        elif x < 0:
+            st.write("Invalid.")
+            break
+
+if stop:
+    st.success("Countdown Finished!")
