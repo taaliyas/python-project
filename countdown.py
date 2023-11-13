87748312806eda3a0857f6df1a5d2251e7044149
gitaa <nagitaaliya07@gmail.com>
2023-11-14 06:52:55 +0700

    updated

--- countdown.py
+++ countdown.py
@@ -12,19 +12,20 @@ start = st.button("Start Countdown")
 stop = st.button("Stop Countdown")
 
 if start:
-    while t:
-        mins, secs = divmod(t, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        st.write(timeformat, end='\r')
-        time.sleep(1)
-        t -= 1
-        if t == 0:
-            st.success("Countdown Finished!")
-            winsound.PlaySound('alarm.wav', winsound.SND_FILENAME)
-            break
-        elif t < 0:
-            st.write("Invalid.")
-            break
+    with st.empty():
+        while t:
+            mins, secs = divmod(t, 60)
+            timeformat = '{:02d}:{:02d}'.format(mins, secs)
+            st.write(timeformat, end='\r')
+            time.sleep(1)
+            t -= 1
+            if t == 0:
+                st.success("Countdown Finished!")
+                winsound.PlaySound('alarm.wav', winsound.SND_FILENAME)
+                break
+            elif t < 0:
+                st.write("Invalid.")
+                break
 
 
 if stop:
