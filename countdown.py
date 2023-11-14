07f8a74d87c9f07631b118bec6b8121d3deb96a4
gitaa <nagitaaliya07@gmail.com>
2023-11-14 07:51:23 +0700

    updated

--- countdown.py
+++ countdown.py
@@ -16,7 +16,7 @@ if start:
         while t:
             mins, secs = divmod(t, 60)
             timeformat = '{:02d}:{:02d}'.format(mins, secs)
-            st.header(timeformat, end='\r')
+            st.title(timeformat)
             time.sleep(1)
             t -= 1
             if t == 0:
